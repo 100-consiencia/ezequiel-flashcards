6c6e9492f02f248f1d8a4812b178ab04596db04c
Ezequiel <141949253+100-consiencia@users.noreply.github.com>
2024-10-09 18:12:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Copa do Mundo',
+    'Qual país sediou a primeira Copa do Mundo?',
+    'A primeira Copa do Mundo foi sediada no Uruguai, em 1930.'
+);
+
+criaCartao(
+    'Copa do Mundo',
+    'Quem é o maior artilheiro da história da Copa do Mundo?',
+    'Marta é a maior artilheira da Copa do Mundo com 17 gols, seguida por Miroslav Klose, com 16 gols.'
+);
+
+criaCartao(
+    'Copa do Mundo',
+    'Quantas Copas do Mundo o Brasil venceu?',
+    'O Brasil venceu 5 Copas do Mundo: 1958, 1962, 1970, 1994 e 2002.'
+);
+
+criaCartao(
+    'Copa do Mundo',
+    'Qual foi a Copa do Mundo com mais gols marcados?',
+    'A Copa do Mundo de 1998, na França, teve 171 gols marcados.'
+);
+
+criaCartao(
+    'Copa do Mundo',
+    'Qual país possui mais títulos da Copa do Mundo?',
+    'O Brasil possui mais títulos da Copa do Mundo, com um total de 5.'
+);
